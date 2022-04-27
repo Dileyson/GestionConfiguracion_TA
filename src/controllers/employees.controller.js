9d4796503a477b45ba0d2db0d3464dc8471455d9
EquirozVasco <esteban.quirozv@gmail.com>
2022-04-27 16:22:41 -0500

    Se crean las nuevas respuestas para probar la imágen de docker

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -2,6 +2,7 @@
  * controlador de empleados
  */
 // const res = require('express/lib/response');
+const { response } = require('express');
 const { Pool } = require('pg');
 const config = require('../config');
 
@@ -14,84 +15,77 @@ const pool = new Pool({
 });
 
 const getEmployees = async (req, res) => {
-  const response = await pool.query('SELECT * FROM employee')
-  res.status(200).json(response.rows);
+  // const response = await pool.query('SELECT * FROM employee')
+  // res.status(200).json(response.rows);
+  const response = await console.log('Lista todos los empleados');
 };
 
 const createEmployee = async (req, res) => {
-  const { name, role, salary, birth } = req.body;
-  try {
-    const response = await pool.query("INSERT INTO employee ( name, role, salary, birth) VALUES ($1, $2, $3, $4)",
-      [name, role, salary, birth]);
-    res.status(201).send({
-      message: "Empleado agregado",
-      body: {
-        employee: { name, role, salary, birth },
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  // const { name, role, salary, birth } = req.body;
+  // try {
+  //   const response = await pool.query("INSERT INTO employee ( name, role, salary, birth) VALUES ($1, $2, $3, $4)",
+  //     [name, role, salary, birth]);
+  //   res.status(201).send({
+  //     message: "Empleado agregado",
+  //     body: {
+  //       employee: { name, role, salary, birth },
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  const response = await console.log('Crea empleado');
 };
 
 const getEmployeeById = async (req, res) => {
-  const id = parseInt(req.params.id);
-  try {
-    const response = await pool.query('SELECT * FROM employee WHERE id = $1', [id]);
-    res.json(response.rows);
-  } catch (error) {
-    console.error(error);
-  }
+  // const id = parseInt(req.params.id);
+  // try {
+  //   const response = await pool.query('SELECT * FROM employee WHERE id = $1', [id]);
+  //   res.json(response.rows);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  const response = await console.log('Lista un empleado');
 }
 
 const updateEmployeeById = async (req, res) => {
-  const id = parseInt(req.params.id);
-  try {
-    const { name, role, salary, birth } = req.body;
-    const response = await pool.query(`UPDATE employee 
-    SET name = $1, 
-    role = $2, 
-    salary = $3, 
-    birth = $4  
-    WHERE id = $5`,
-      [name, role, salary, birth, id]
-    )
-    res.status(201).send({
-      message: "Empleado Actualizado",
-      body: {
-        employee: { name, role, salary, birth },
-      },
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  // const id = parseInt(req.params.id);
+  // try {
+  //   const { name, role, salary, birth } = req.body;
+  //   const response = await pool.query(`UPDATE employee 
+  //   SET name = $1, 
+  //   role = $2, 
+  //   salary = $3, 
+  //   birth = $4  
+  //   WHERE id = $5`,
+  //     [name, role, salary, birth, id]
+  //   )
+  //   res.status(201).send({
+  //     message: "Empleado Actualizado",
+  //     body: {
+  //       employee: { name, role, salary, birth },
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  const response = await console.log('Actualiza empleado');
 }
 
 const deleteEmployeeById = async (req, res) => {
-  const id = parseInt(req.params.id);
-  try {
-    const response = await pool.query('DELETE FROM employee WHERE id = $1', [id]);
-    res.status(200).send({ message: "Empleado borrado" });
-  } catch (error) {
-    console.error('deleteEmployeeById', error);
-    res.status(500).send({
-      message: "Ocurrio un error"
-    });
-  }
+  // const id = parseInt(req.params.id);
+  // try {
+  //   const response = await pool.query('DELETE FROM employee WHERE id = $1', [id]);
+  //   res.status(200).send({ message: "Empleado borrado" });
+  // } catch (error) {
+  //   console.error('deleteEmployeeById', error);
+  //   res.status(500).send({
+  //     message: "Ocurrio un error"
+  //   });
+  // }
+  const response = await console.log('Elimina un empleado');
 }
 
-// ==> Método eliminar un empleado con un id dado:
-exports.deleteEmployeeById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await db.query("DELETE FROM employee WHERE id = $1", [id]);
-    res.status(200).send({ message: "Empleado borrado" });
-  } catch (error) {
-    console.error('deleteEmployeeById', error);
-    res.status(500).send({
-      message: "Ocurrio un error"
-    });
-  }
-};
+
 
 module.exports = { getEmployees, createEmployee, getEmployeeById, updateEmployeeById, deleteEmployeeById }
